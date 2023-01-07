6c0c0fe18fd519597004b1e18a45e097f43b9493
AhmedDWIIM <91135844+AhmedDWIIM@users.noreply.github.com>
2023-01-07 22:38:34 +0100

    Login - Form id/mdp - problem submit

--- bot.js
+++ bot.js
@@ -1,14 +1,32 @@
 const puppeteer = require('puppeteer');
+const dotenv = require('dotenv');
+dotenv.config();
 
 async function run() {
     const browser = await puppeteer.launch({
         headless: false
     });
 
-    const page = browser.newPage();
-
+    const page = await browser.newPage();
+    // Aller sur la page d'accueil d'Instagram
     await page.goto('https://www.instagram.com/');
-    await page.waitForSelector('body', { timeout : 3000});
+    // Accepter les cookies
+    await page.waitForSelector('button[class="_a9-- _a9_0"]');
+    await page.click('button[class="_a9-- _a9_0"]');
+
+    // Attente de l'affichage du formulaire de connexion
+    await page.waitForSelector('form',{timeout:3000});
+    // Saisie de l'identifiant
+    await page.type('input[name=username]', process.env.INSTAGRAM_ID);
+    // Saisie du mot de passe
+    await page.type('input[name=password]', process.env.INSTAGRAM_PASSWORD);
+    // Soumission du formulaire
+    try {
+        // Soumission du formulaire
+        await page.click('button[class="_acan _aiit _acap _aijb _acas _aj1-"]');
+      } catch (error) {
+        console.error(error);
+      }
 }
 
 run();
